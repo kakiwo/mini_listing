b8cb2e4e1de35adc84ebdce9ca06975a9be01a08
kakiwo <kakiwo@hotmail.co.jp>
2020-02-07 13:54:12 +0900

    create-images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    @products = Products.includes(:images).order('created_at DESC')
+    @products = Product.includes(:images).order('created_at DESC')
   end
 
   def new
+    @product = Product.new
+    @product.images.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,5 +24,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, images_attributes: [:src])
   end
 end
